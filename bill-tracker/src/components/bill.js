e7dcb7e9d55c00332d255ac321bb985bb0461a9c
Michael Arteaga <michaelarteaga34@Gmail.com>
2020-05-23 22:10:35 -0700

    added date pickers

--- bill-tracker/src/components/bill.js
+++ bill-tracker/src/components/bill.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function Bill() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
     <div>
       <form>
         <input name="bill name" type="text" placeholder="Bill"></input>
         <input name="bill cost" type="text" placeholder="Cost"></input>
         <input name="bill provider" type="text" placeholder="Provider"></input>
-        <input name="bill due date" type="text" placeholder="Due Date"></input>
-        <input
-          name="bill date paid"
-          type="text"
-          placeholder="Date Paid"
-        ></input>
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => {
+            setSelectedDate(date);
+          }}
+          placeholderText="Due Date"
+          required
+        />
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => {
+            setSelectedDate(date);
+          }}
+          placeholderText="Date Paid"
+          required
+        />
         <input name="bill username" type="text" placeholder="Username"></input>
         <input name="bill password" type="text" placeholder="Password"></input>
         <input
--- bill-tracker/src/components/months.js
+++ bill-tracker/src/components/months.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Bill from "./bill";
 import Addbillbtn from "./buttons/addbillbtn";
 
 const Months = () => {
-  const [bill, setBill] = useState([]);
-
   return (
     <div>
       <h2>Month1</h2>
